c93f5b28a557105c0293b5207d49a6199b1e9cd2
dodo <81587543@qq.com>
2020-11-15 14:19:54 +0800

    Merge remote-tracking branch 'origin/test' into test

--- src/Main.py
+++ src/Main.py
@@ -5,7 +5,6 @@ from db.impl.MariadbConnectionImpl import MariadbConnectionImpl
 
 if __name__ == '__main__':
     # 首先需要config/config.ini修改数据库信息
-
     projectConfig = ConfigHelper.getProjectConfig()
     conn = MariadbConnectionImpl().getConnection()
     # 获取数据库链接,和项目信息传递给生成器
